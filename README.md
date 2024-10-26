2a36c3d9863d6308498dc2bf8a4607c9fb2c45fc
Ahmed Maher Algohary <ahmedmaheraljwhry057@gmail.com>
2024-10-27 01:34:43 +0300

    completed the game's logic, added logo to the app, modified the game design

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# similarity-game__test-your-memory
+# Similarity Game (Memory Game)
+A fun and interactive card-matching game designed to test and improve memory skills. Built using HTML, CSS, JavaScript, and Bootstrap, this game provides a user-friendly interface and a responsive design for an engaging experience on various devices. The game challenges players to match pairs of cards with minimal attempts, keeping track of their progress and performance. Perfect for casual gaming, brain training, or learning purposes.
+
+
+### Used Technologies:
+- **HTML**
+- **CSS**
+- **JavaScript (ES6+)**
+- **Bootstrap**
+- **Other Libraries:** font awesome
+
+
+### Demo (Live Preview):
+Feel free to play the game: <a href="" target="_blank" title="Play the game"></a>
+
+
+
+### Key Features:
+- **Interactive Gameplay:** Players flip cards to find matching pairs, testing their memory skills.
+- **Randomized Setup:** Each game session starts with a shuffled set of cards, ensuring a new challenge every time.
+- **Responsive Design:** Built with a mobile-first approach, the game works seamlessly across different screen sizes, including mobile devices and - desktops.
+- **Score Tracking:** Keeps track of the number of attempts made to match all pairs, encouraging players to improve their performance.
+- **Feedback Messages:** Displays success or failure messages based on the player's progress, providing instant feedback.
+- **Smooth Animations:** Animations enhance the user experience, such as card flips and popup messages.
+- **Pop-up Modals:** Uses pop-up modals to display results and game-related information in an engaging manner.
+- **Cross-Browser Compatibility:** The game is tested and works well in all modern web browsers.
+
+
+### Instructions for Local Setup
+To run this project locally, follow these steps:
+1. Clone the repository:
+=> git clone https://github.com/Ahmed-Maher77/similarity-game__test-your-memory.git
+2. Open the project folder (similarity-game__test-your-memory)
+3. Open the **index.html** file in your browser to start the game.
+
--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
-// array of cards
 let attempts = 5;
 let matched_pairs = 0;
+let gameStarted = false;
+// flag to temporarily disable clicks while the match check is in progress
+let isCheckingMatch = false;
+const restartGame_btns = document.querySelectorAll(".restart-game");
+
+
 class Card {
-    constructor(id, symbol) {
-        this._id = id;
-        this._symbol = symbol;
-    }
+	constructor(id, symbol) {
+		this.id = id;
+		this.symbol = symbol;
+	}
 }
+
+
+// array of cards
 const cards = [
 	new Card(1, "⭐"),
 	new Card(1, "⭐"),
@@ -15,42 +23,50 @@ const cards = [
 	new Card(3, "🚀"),
 	new Card(3, "🚀"),
 	new Card(4, "🐱"),
-	new Card(4, "🐱")
+	new Card(4, "🐱"),
+	new Card(5, "🤷‍♀️"),
+	new Card(5, "🤷‍♀️")
 ];
 
 
 // Attempts
-const heartsContainer = document.querySelector('.hearts');
+const heartsContainer = document.querySelector(".hearts");
 function showHearts() {
-    heartsContainer.innerHTML = '';
-    for (let i = 0; i < attempts; i++) {
-        const heart = document.createElement('i');
-        heart.className = "fa-solid fa-heart fs-4 text-danger pe-1";
-        heartsContainer.appendChild(heart);
-    }
-    if (attempts <= 0) {
-        setTimeout(() => {
-            failMsg()
-        }, 500);
-    }
+	heartsContainer.innerHTML = "";
+	for (let i = 0; i < attempts; i++) {
+		const heart = document.createElement("i");
+		heart.className = "fa-solid fa-heart fs-5 text-danger pe-1";
+		heartsContainer.appendChild(heart);
+	}
+	if (attempts <= 0) {
+		setTimeout(() => {
+			failMsg();
+		}, 500);
+	}
 }
 showHearts();
 
 
 // Matched Pairs
-const matchedPairsSpan = document.querySelector('.matched-pairs span');
+const matchedPairsSpan = document.querySelector(".matched-pairs span");
 function showMatchedPairs() {
-    matchedPairsSpan.innerText = matched_pairs;
-    successMsg()
+	matchedPairsSpan.innerText = matched_pairs;
+	if (matched_pairs >= cards.length / 2) {
+		successMsg();
+	}
 }
-showMatchedPairs()
+showMatchedPairs();
 
 
-// loop over cards >> create each card >> append to its parent
+// arrange cards randomly
+const shuffleCards = (els) => els.sort(() => 0.5 - Math.random());
+
+
+// loop over cards >> create each card >> append them to their parent
 const cardsContainer = document.querySelector(".cards");
-cards.forEach((card) => {
+shuffleCards(cards).forEach((card) => {
 	cardsContainer.innerHTML += `
-        <div class="col-6 col-sm-4 col-lg-3 col-xl-2">
+        <div class="col-4 col-sm-3 col-lg-2">
             <div class="card border h-300" data-id='${card.id}'>
                 <div class="front-face font-big bg-secondary h-100 w-100 d-flex justify-content-center align-items-center px-2 rounded">
                     <p>${card.symbol}</p>
@@ -61,94 +77,148 @@ cards.forEach((card) => {
     `;
 });
 
+
 // when click the start button >> change its content [start a new game], its color [bg-danger]
-const startBtn = document.getElementById('start-btn');
-const cardElements = cardsContainer.querySelectorAll('.card');
-startBtn.addEventListener('click', () => {
-    // flip all cards
-    cardElements.forEach(card => {
-        card.classList.add('active');
-    });
-    // change the button content
-    startBtn.innerText = 'Restart the Game';
-    startBtn.classList.replace('bg-primary', 'bg-danger');
-    // Reset cards after 2s
-    setTimeout(() => {
-        resetCards()
-    }, 2000);
-})
+const startBtn = document.getElementById("start-btn");
+const cardElements = cardsContainer.querySelectorAll(".card");
+startBtn.addEventListener("click", () => {
+	if (!gameStarted) {
+		flipCards();
+		// change the button content
+		startBtn.innerHTML = `<i class="fa-solid fa-rotate-left"></i> Restart the Game`;
+		startBtn.classList.replace("rounded-pill", "rounded");
+		startBtn.classList.replace("bg-primary", "bg-danger");
+
+		// Reset cards after 2s
+		setTimeout(() => {
+			resetCards();
+			gameStarted = true;
+		}, 1000);
+	} else {
+		restartGame();
+	}
+});
+
+// Adjust Cusror Status
+cardElements.forEach((card) => {
+	card.onmouseover = () => {
+		if (gameStarted && !card.classList.contains("active")) {
+			card.style.cursor = "pointer";
+		} else {
+			card.style.cursor = "not-allowed";
+		}
+	};
+});
+
+// flip all cards
+function flipCards() {
+	cardElements.forEach((card) => {
+		card.classList.add("active");
+	});
+}
 
 // Reset Cards
 function resetCards() {
-    cardElements.forEach(card => {
-        card.classList.remove('active');
-    })
+	cardElements.forEach((card) => {
+		card.classList.remove("active");
+	});
 }
 
 // when click on each card
 let firstCard, secondCard;
-cardElements.forEach(card => {
-    card.onclick = function() {
-        card.classList.add('active');
-        if (!firstCard) {
-            firstCard = card
-        } else {
-            secondCard = card
-        };
+cardElements.forEach((card) => {
+	card.onclick = function () {
+		if (gameStarted  && !isCheckingMatch && card.getAttribute("data-match") !== "matched") {
+			card.classList.add("active");
+			if (!firstCard) {
+				firstCard = card;
+			} else {
+				secondCard = card;
+                isCheckingMatch = true;
+			}
 
-        if (card.getAttribute('data-match') !== 'matched') {
-            if (firstCard && secondCard) isMatch()
-        }
-        
-    }
-})
+			if (firstCard && secondCard) isMatch();
+		}
+	};
+});
 
 // to check if the 2 cards are matched or not
 function isMatch() {
-    const firstCard_id = firstCard.getAttribute('data-id');
-    const secondCard_id = secondCard.getAttribute('data-id');
-    if (firstCard_id === secondCard_id) {
-        matched_pairs++;
-        showMatchedPairs();
-        matchedPairsSpan.classList.add('text-success');
-        firstCard.querySelector('.front-face').classList.add('bg-green');
-        secondCard.querySelector('.front-face').classList.add('bg-green');
-        firstCard.setAttribute('data-match', 'matched');
-        secondCard.setAttribute('data-match', 'matched');
-        console.log(secondCard);
-        
-    } else {
-        setTimeout(() => {
-            firstCard.classList.remove('active');
-            secondCard.classList.remove('active');
-        }, 500);
-        attempts--;
-        showHearts()
-    }
-    // Reset variables
-    setTimeout(() => {
-        firstCard = null;
-        secondCard = null;
-    }, 500);
+	const firstCard_id = firstCard.getAttribute("data-id");
+	const secondCard_id = secondCard.getAttribute("data-id");
+	if (firstCard_id === secondCard_id) {
+		matched_pairs++;
+		showMatchedPairs();
+		matchedPairsSpan.classList.add("text-success");
+		firstCard.querySelector(".front-face").classList.add("bg-green");
+		secondCard.querySelector(".front-face").classList.add("bg-green");
+		firstCard.setAttribute("data-match", "matched");
+		secondCard.setAttribute("data-match", "matched");
+	} else {
+		setTimeout(() => {
+			firstCard.classList.remove("active");
+			secondCard.classList.remove("active");
+		}, 500);
+		attempts--;
+		showHearts();
+	}
+	// Reset variables
+	setTimeout(() => {
+		firstCard = null;
+		secondCard = null;
+        isCheckingMatch = false;
+	}, 500);
 }
 
 
+const failMatchedPairs = document.querySelector('.fail-matched-pairs');
 // Fail Message
 function failMsg() {
-    // You lost 😥
-    // Matched Pairs
-    // Try Again
+	const triggerFail = document.getElementById("trigger-fail");
+	const failAttempts = document.querySelector(".fail-attempts");
+	failAttempts.innerText = 5 - attempts;
+	failMatchedPairs.innerText = matched_pairs;
+	triggerFail.click();
 }
 
 // Success Message
-function showMatchedPairs() {
-
+function successMsg() {
+	const triggerSuccess = document.getElementById("trigger-success");
+	const successAttempts = document.querySelector(".success-attempts");
+	successAttempts.innerText = 5 - attempts;
+	triggerSuccess.click();
 }
 
+const closeFailMsg = document.querySelector('.close-fail-msg');
+const closeSuccessMsg = document.querySelector('.close-success-msg');
+restartGame_btns.forEach(btn => {
+	btn.onclick = () => {
+		restartGame();
+		closeFailMsg.click();
+		closeSuccessMsg.click();
+	};
+})
 
-// when click on [start a new game]
-
-// when click on any card before starting the game
-
-// attempts <= 0   => pop-up
-// matched_pairs >= cards.length / 2   => pop-up
+// Restart the game
+function restartGame() {
+	attempts = 5;
+	showHearts();
+	matched_pairs = 0;
+	matchedPairsSpan.classList.remove("text-success");
+	showMatchedPairs();
+	gameStarted = false;
+    firstCard = null;
+    secondCard = null;
+    // Reset the card elements
+	cardElements.forEach((card) => {
+        card.classList.remove("active");
+		card.querySelector(".front-face").classList.remove("bg-green");
+		card.setAttribute("data-match", "");
+	});
+    // Reset the start button
+    startBtn.innerHTML = `<i class="fa-solid fa-play"></i> Start Game`;
+    startBtn.classList.replace("bg-danger", "bg-primary");
+    startBtn.classList.replace("rounded", "rounded-pill");
+    // shuffleCards(cards);
+	startBtn.click();
+}
